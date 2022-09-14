8c91e6edb27ba7d870cd63dd7d86afd410b44112
Baristopcu08 <baristopcu2008@gmail.com>
2022-09-14 22:46:17 +0300

    HW004/end

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -3,9 +3,7 @@ import java.security.PublicKey;
 public class SpeedConverter {
     //TODO: Please open the readme.txt file and answer 1st and 2nd questions.
 
-       //TODO /*DİKKAT
-        /*BEN BU MİLL HESAPLAMA KATSAYISI VE MİLL SAAT KONUSUNU ANLAMADIM.
-        ALGORİTMAM DOĞRU OLABİLİR FAKAT SONUÇLARIN HATALIDIR*/
+
 
     public static void main(String[] args) {
 
